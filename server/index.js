8a8e7aafebdc74c2ab754228b60aab3a3be75df0
kushalch99 <kushalch99@gmail.com>
2021-07-27 11:52:01 +0530

    server initialized

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const cors = require('cors')
+require('./sequelize.js')
+
+var app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello')
+})
+
+app.listen(process.env.PORT, function () {
+  console.log('Example app listening on port: ' + process.env.PORT);
+})
